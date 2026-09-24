6c4478e0a165b0240e8b857bb62ec9e06adec81a
tolu <gbemilekeogidan@gmail.com>
2026-09-24 10:10:24 +0100

    adding user to a group with subscription "reader" role

--- vending/dev/identity.tf
+++ vending/dev/identity.tf
@@ -7,3 +7,12 @@ resource "azuread_group" "platform_readers" {
   display_name     = "grp-leke-platform-readers"
   security_enabled = true
 }
+
+data "azuread_user" "tolu" {
+  user_principal_name = "tolu@gbemilekeogidanoutlook.onmicrosoft.com"
+}
+
+resource "azuread_group_member" "tolu_platform_reader" {
+  group_object_id  = azuread_group.platform_readers.object_id
+  member_object_id = data.azuread_user.tolu.object_id
+}
